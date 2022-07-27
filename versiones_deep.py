d0a4b738710a113e4d85fa6e07f10c2e8fbf562b
Jhonny Osorio Gallego <66349163+josorio398@users.noreply.github.com>
2022-07-27 18:35:49 -0500

    subí todos los archivos

---
+++ versiones_deep.py
@@ -0,0 +1,10 @@
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
+
+# keras
+import matplotlib
+print('matplotlib: %s' % keras.__version__)
